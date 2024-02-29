a22b6bdd61f70820b36e726b95c8b01fb7e67d26
jakynevs <johnnevilleis@hotmail.co.uk>
2024-02-29 16:50:30 +0100

    Improved error handling and centralised wait time in file

--- scraper.py
+++ scraper.py
@@ -4,42 +4,44 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from analyser import analyse_text
 
 def scrape_and_analyse(url):
+    WAIT_TIME = 10  # Centralized wait time
+
     # specifies the path to the chromedriver.exe
     driver_path = dp
     service = Service(executable_path=driver_path)
     driver = webdriver.Chrome(service=service)  
 
-    # Load website   
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)  # Wait up to 10 seconds
-    close_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="artdeco-icon lazy-loaded"]')))
-    
     try:
-        close_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@class="artdeco-icon lazy-loaded"]'))
-        )
-        print("Found close button. Clicking it...")
-        close_button.click()
-    except Exception as e:
-        print("Close button not found or error clicking it:", e)
+        # Load website   
+        driver.get(url)
+        wait = WebDriverWait(driver, WAIT_TIME) 
 
-        # Example: Adjust the wait time and condition as needed for your specific case
-    try:
+        # Attempt to close pop-up
+        try:
+            close_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="artdeco-icon lazy-loaded"]')))
+            close_button.click()
+
+        except (NoSuchElementException, TimeoutException) as e:
+            print("Close button not found or error clicking it:", e)
+
+            # Example: Adjust the wait time and condition as needed for your specific case
+    
         # Wait for the About section to be loaded
-        about_section = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="main-content"]/section[1]/div/section[1]/div/p'))
-        )
+        about_section = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main-content"]/section[1]/div/section[1]/div/p')))
         about_section_text = about_section.text
-    
-    except Exception as e:
-        result = "About section not found."
-        print(e)
+        result = analyse_text(about_section_text)
 
-    result = analyse_text(about_section_text)
+    except Exception as e:
+        print("An error occurred:", e)
+        result = "Error processing page."
     
+    finally:
+        driver.quit()
+
     return result, about_section_text
 
     
