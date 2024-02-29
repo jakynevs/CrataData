0c3eaef889718ec3e14ac1283d376e093ebf7bc8
jakynevs <johnnevilleis@hotmail.co.uk>
2024-02-29 15:37:23 +0100

    Changed how app.py deals with scrape_and_analyse result

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    result = ""
+    result_text = ""
     about_section_text = ""
 
     if request.method == 'POST':
         url = request.form['linkedin_url']
         result, about_section_text = scrape_and_analyse(url)
-    return render_template('index.html', scrape_and_analyse_result=result, about_section=about_section_text)
+        if result[0] == 1:
+            result_text = 'Yes'
+        else:
+            result_text = 'No'
+
+    return render_template('index.html', scrape_and_analyse_result=result_text, about_section=about_section_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
